f309afc19179884d4c43b2f7cc84c419940caa08
Malick WANE <mwane@e2r5p13.42.fr>
2019-10-10 17:02:18 +0200

    add strsub.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mwane <mwane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/10 15:50:37 by mwane             #+#    #+#             */
-/*   Updated: 2019/10/10 15:51:11 by mwane            ###   ########.fr       */
+/*   Created: 2019/10/10 15:50:03 by startne           #+#    #+#             */
+/*   Updated: 2019/10/10 16:38:03 by mwane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strsub(const char *s, unsigned int start, size_t len)
+{
+	char	*newstr;
+
+	if (!(newstr = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	start += - 1;
+	while (s[++start] && start < len)
+	{
+		newstr[start] = s[start];
+	}
+	newstr[start] = '\0';
+	return (newstr);
+}
 
