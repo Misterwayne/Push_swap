e19722a54541985e66caf83f30ce954a7391dfd8
Malick WANE <mwane@e2r5p14.42.fr>
2019-10-11 13:49:59 +0200

    fix substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,25 +6,27 @@
 /*   By: mwane <mwane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 15:50:03 by startne           #+#    #+#             */
-/*   Updated: 2019/10/10 16:38:03 by mwane            ###   ########.fr       */
+/*   Updated: 2019/10/11 13:49:34 by mwane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strsub(const char *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	char	*newstr;
+	char		*newstr;
+	size_t		i;
 
+	i = 0;
 	if (!(newstr = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	start += - 1;
-	while (s[++start] && start < len)
+	if (s == NULL)
+		return (newstr);
+	while (s[start] && i + 1 <= len)
 	{
-		newstr[start] = s[start];
+		newstr[i++] = s[start++];
 	}
-	newstr[start] = '\0';
+	newstr[i] = '\0';
 	return (newstr);
 }
-
