eed10fdf3b38a67e581f2caca82f5dbd0cb06fee
Malick WANE <mwane@e2r5p11.42.fr>
2019-10-17 11:23:05 +0200

    lcpy fixed

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mwane <mwane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 16:52:12 by mwane             #+#    #+#             */
-/*   Updated: 2019/10/10 16:04:32 by mwane            ###   ########.fr       */
+/*   Updated: 2019/10/17 11:20:46 by mwane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ int		ft_strlcpy(char *dest, const char *src, int size)
 	int i;
 
 	i = 0;
-	while (i < size)
+	if (size == 0)
+		return (ft_strlen((char *)src));
+	while (i < size - 1 && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	i = 0;
-	while (src[i])
-		i++;
-	return (i);
+	return (ft_strlen((char *)src));
 }
