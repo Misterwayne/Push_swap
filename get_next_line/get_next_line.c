0a2924846b699628b5cc35091dfb56d478eb953b
42 <42>
2019-10-27 16:48:23 +0100

    we moving

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mwane <mwane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 11:11:15 by mwane             #+#    #+#             */
-/*   Updated: 2019/10/25 16:18:09 by mwane            ###   ########.fr       */
+/*   Updated: 2019/10/27 16:25:21 by mwane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,90 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char     *reallocbuff(char *buffer, char *str, int size, int i)
+char     *reallocbuff(char *buffer, char *str, int size, int i, int z)
 {
 	int j;
-	char *tmp;
+	int x;
+	//static int k = 1;
+	static char *tmp;
 
-	j = -1;
+	j = 0;
+	x = 0;
 	tmp = malloc(sizeof(char) * ((size * i) + 1));
-	while (buffer[j++])
-		str[size++ * (i - 1)] = buffer[j];
+	while (str[z])
+		tmp[j++] = str[z++];
+	while (buffer[x] && buffer[x] != '\n')
+		tmp[j++] = buffer[x++];
+	tmp[j] = '\0';
 	if (str)
 		free(str);
 	str = tmp;
 	return (str);
 }
 
-int		scan_buffer(char *buff)
+int		scan_buffer(char *buff,char *overbuff)
 {
-	while (*buff)
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (buff[i])
 	{
-		if (*buff == '\n')
+		if (buff[i] == '\n')
 		{
-			*buff = '\0';
+			buff[i] = '\0';
+			if (buff[i + 1])
+			{
+				while(buff[++i])
+					overbuff[j++] = buff[i];
+				overbuff[j] = '\0';
+			}
+			printf("over = %s\n",overbuff);
 			return (1);
 		}
 		else
-			buff++;
+			i++;
 	}
 	return (0);
+}
+
+int filloverbuff(char *overbuff,char *line)
+{
+	int i;
+
+	i = 0;
+	while (overbuff[i])
+		*line++ = overbuff[i++];
+	return i;
 }
 
 int get_next_line(int fd, char **line)
 {
 	char *buffer;
-	static char *test;
+	char *overbuff;
 	int count;
+	int	x = 0;
 	int i = 1;
 
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	line = malloc(sizeof(char *) * 1);
 	*line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	overbuff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	while ((count = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (!scan_buffer(buffer))
+		buffer[count] = '\0';
+		if (!scan_buffer(buffer,overbuff))
 		{
-			reallocbuff(buffer, test, BUFFER_SIZE, i);
+			*line = reallocbuff(buffer, *line, BUFFER_SIZE, i, x);
 			i++;
 		}
 		else
-			return 0;
+		{
+			//x += filloverbuff(overbuff, *line);
+			*line = reallocbuff(buffer, *line, BUFFER_SIZE, i, x);
+			return count;
+		}
 	}
-	buffer[BUFFER_SIZE + 1] = '\0';
-	while (i < BUFFER_SIZE)
-	{
-		(*line)[i] = buffer[i];
-		i++;
-	}
-	buffer[BUFFER_SIZE + 1] = '\0';
-	(*line)[BUFFER_SIZE + 1] = '\0';
-	printf("%s",*line);
 	return 0;
 }
 
@@ -83,9 +108,16 @@ int get_next_line(int fd, char **line)
 
 int main(void)
 {
-	int fd = 0; 
+	int fd = 0;
+	char	*line;
 	
+	line = NULL;
 	fd = open("test.txt", O_RDONLY);
 
-	get_next_line(fd,NULL);
+	get_next_line(fd, &line);
+	printf("%s\n\n",line);
+	get_next_line(fd, &line);
+	//{
+		printf("%s\n\n",line);
+	//}
 }
