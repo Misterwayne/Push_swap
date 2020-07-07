62a0e50d33b93c474a2ae19ff0419714a5416788
Misterwayne <baidtfall319@gmail.com>
2020-07-07 18:02:54 +0200

    cleaned

--- ex05/kata02.py
+++
@@ -1,3 +0,0 @@
-date = (3,30,2019,9,25)
-
-print(str(date[3]) + "/" + str(date[4]) + "/" + str(date[2]) + ' ' + str(date[0]) + ":" + str(date[1]))
--- ex05/kata03.py
+++
@@ -1,7 +0,0 @@
-phrase = "The right format"
-
-res = ''
-
-while (len(res + phrase) < 41):
-    res += '-'
-print(res+phrase)
